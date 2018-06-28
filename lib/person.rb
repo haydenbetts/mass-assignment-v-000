8943d713a51e9a1ca7237c6fc4eebb8ae53f882d
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 23:24:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,11 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-  :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize
+  def initialize(new_user_details)
+    new_user_details.each do |detail, value|
+      self.send("#{detail}=", value)
+    end
   end
 end
